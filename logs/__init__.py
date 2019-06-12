8bcc8771705deea8186d4b524c44ab7489e0a0c3
Zhang, XinhuiX <xinhuix.zhang@intel.com>
2019-06-12 22:11:08 +0800

    update "logging"

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,19 +1,12 @@
 import os
 import logging
-import os
 from logging.handlers import RotatingFileHandler
 from conf import ConfManagement
 
 
 class DevelopmentConfig(object):
-    DEBUG = ConfManagement().get_ini("debug_format")
-    log_match = {
-        "info": logging.INFO,
-        "debug": logging.DEBUG,
-        "error": logging.ERROR,
-        "warning": logging.WARNING
-    }
-    LOG_LEVEL = log_match.get(DEBUG)
+    DEBUG = ConfManagement("default").get_ini("DEBUG")
+    LOG_LEVEL = logging.DEBUG
 
 
 class SetLog(object):
@@ -23,15 +16,12 @@ class SetLog(object):
         log_path = os.path.dirname(os.path.realpath(__file__))
         log = os.path.join(log_path, "project.log")
         file_log_handler = RotatingFileHandler(
-<<<<<<< HEAD
             log, maxBytes=1024 * 1024 * 100, backupCount=10)
-=======
-        log, maxBytes=1024 * 1024 * 100, backupCount=10)
->>>>>>> ae1124d7c678b958082ae4872a55f9e190414c9e
         # formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_log_handler.setFormatter(formatter)
         logging.getLogger().addHandler(file_log_handler)
+
     def __getattr__(self, e):
         self.setup_log()
         log_match = {
