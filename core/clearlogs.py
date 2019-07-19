ee594f99bef003b45cc923d86562581736a09e8b
ITjiuzhanggui <zhangxinhuicf@163.com>
2019-07-19 16:17:17 +0800

    update clr flink "W"

--- core/clearlogs.py
+++ core/clearlogs.py
@@ -1362,6 +1362,6 @@ class ClrFlink(ClrTestLog):
                 data.get("clear").get("flink").update(
                     {"StreamNetworkLatencyBenchmarkExecutor.networkLatency1to1": num[-2]})
 
-        with open(self.json_path, 'w')as f:
+        with open(self.json_path, "w")as f:
             json.dump(data, f)
 
