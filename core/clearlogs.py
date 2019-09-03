6d31aad0a2d92d10632660e286b0969c8fc52009
ITjiuzhanggui <zhangxinhuicf@163.com>
2019-09-03 11:46:45 +0800

    update run_script.py

--- core/clearlogs.py
+++ core/clearlogs.py
@@ -1304,12 +1304,12 @@ class ClrFlink(ClrTestLog):
         lines = self.test_log
         data = self.data
 
-        # for i in lines[
-        #          lines.index("[flink] [INFO] Test clear docker image:\n"):
-        #          lines.index("Clr-Flink-Server\n")]:
         for i in lines[
-                 lines.index("[flink] [INFO] use openjdk12:\n"):
-                 lines.index("Clr-Flink-Server-jdk12\n")]:
+                 lines.index("[flink] [INFO] Test clear docker image:\n"):
+                 lines.index("Clr-Flink-Server\n")]:
+        # for i in lines[
+        #          lines.index("[flink] [INFO] use openjdk12:\n"):
+        #          lines.index("Clr-Flink-Server-jdk12\n")]:
 
             if i.startswith("KeyByBenchmarks.arrayKeyBy"):
                 num = re.findall("\d+\.?\d*", i)
